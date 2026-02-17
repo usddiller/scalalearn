bd33a4a6644c5feeb40a419e715831de871d32f6
Usddiller <grutt2007@gmail.com>
2026-02-17 16:15:37 +0500

    third task

--- src/main/scala/com/rockthejvm/part2oop/OOBasics.scala
+++ src/main/scala/com/rockthejvm/part2oop/OOBasics.scala
@@ -12,6 +12,28 @@ object OOBasics {
     def isWrittenBy(au: Writer) = author == au
     def copy(newYear:Int) = new Novel(name,newYear,author)
   }
+
+  class Counter(count:Int) {
+    def decrement() = {
+      if count >0 then
+        new Counter(count - 1)
+      else this
+    }
+    def increment() = new Counter(count +1)
+
+    def increment(n: Int): Counter =
+      if  n <= 0 then this
+      else increment().increment(n - 1)
+
+    def decrement(n: Int): Counter =
+      if n <= 0 then this
+      else decrement().decrement(n - 1)
+
+//    def increment(n: Int) = if n > 0 then new Counter(count + n) else this
+//    def decrement(n: Int) = if count > n && n > 0 then new Counter(count - n) else new Counter(0)
+    def print() = count
+  }
+
   def main(args: Array[String]): Unit = {
     val charlesDickens = new Writer("Charles","Dickens", 1812)
     val notCharlesDickens = new Writer("Charles", "Dickens", 1811)
